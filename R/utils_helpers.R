3c21e890f695123b6a1ab80afbf4c469bc4497da
Lambert Patrick <patrick.mh.lambert@inrae.fr>
2023-04-19 17:35:56 +0200

    clean mssimplify
    issue #67

--- R/utils_helpers.R
+++ R/utils_helpers.R
@@ -224,8 +224,8 @@ generate_datasets <- function(con) {
     query = "SELECT * FROM diadesatlas.v_ices_geom;"
   ) %>%
     # sf::st_transform("+proj=eqearth +wktext") %>%
-    sf::st_transform("+proj=wintri") %>%
-    rmapshaper::ms_simplify()
+    sf::st_transform("+proj=wintri") # %>%
+    # rmapshaper::ms_simplify()
   
   species_order <- c(
     "Alosa alosa",
